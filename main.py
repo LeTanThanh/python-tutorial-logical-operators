18b44c1e4123409ac126987ec3c63ce23e8fbf3f
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 23:13:58 +0700

    The or operator

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
   price = 9.99
   print(price > 9 and price < 10)
   print(price > 10 and price < 20)
+
+  # The or operator
+
+  """
+  a or b
+  """
+
+  price = 9.99
+  print(price > 10 or price < 20)
+  print(price > 10 or price < 5)
